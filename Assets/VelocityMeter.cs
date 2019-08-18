f988a3a1582ab0617f1a965efe53779e8931eeb7
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-08-18 15:30:31 -0700

    The velocity meter now loops moving up and down

---
+++ Assets/VelocityMeter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VelocityMeter : MonoBehaviour
+{
+    private Slider velocityMeter;
+
+    public float velocitySpeed;
+
+    Coroutine moveUp;
+    Coroutine moveDown;
+
+    public bool movingUp;
+
+    private void Start()
+    {
+        velocityMeter = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if (velocityMeter.value >= -1f)
+        {
+            movingUp = true;
+
+            print("Please move up");
+            // velocityMeter.value -= velocitySpeed * Time.deltaTime;
+            moveUp = StartCoroutine(MoveMeterUp());
+            if (moveDown != null)
+                StopCoroutine(moveDown);
+        }
+        else if (velocityMeter.value <= -250f)
+        {
+            movingUp = false;
+            print("Please move down");
+            moveDown = StartCoroutine(MoveMeterDown());
+            StopCoroutine(moveUp);
+            // StartCoroutine(MoveMeterDown());
+        }
+    }
+
+    IEnumerator MoveMeterUp()
+    {
+        yield return new WaitForSecondsRealtime(.001f);
+        print(velocityMeter.value + " move up");
+        velocityMeter.value += -velocitySpeed;
+
+        if (velocityMeter.value >= -250 && movingUp)
+        {
+            StartCoroutine(MoveMeterUp());
+        }
+
+    }
+
+    IEnumerator MoveMeterDown()
+    {
+        yield return new WaitForSecondsRealtime(.001f);
+        print(velocityMeter.value + " move down");
+        velocityMeter.value += velocitySpeed;
+
+        if (velocityMeter.value <= -1 && movingUp == false)
+        {
+            StartCoroutine(MoveMeterDown());
+        }
+
+    }
+}
